70418e20d0e8a461a49ded8b10f3ce453dc7e658
AllFather88 <marksumilo@gmail.com>
2025-04-27 01:11:51 +0300

    add: lot page

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -9,6 +9,7 @@ import Register from "./pages/registration/reg.js";
 import Auth from "./pages/authentication/auth.js";
 import Main from "./pages/mainPage/main.js";
 import NotFound from "./pages/notFound/notfound.js";
+import Lot from "./pages/mainPage/modules/lot.js";
 
 export const App = () => (
   	<BrowserRouter forceRefresh={true}>
@@ -17,7 +18,8 @@ export const App = () => (
 				<Route path="/*" element={<NotFound/>}/>
         		<Route path="/reg" element={<Register />} />
 				<Route path="/auth" element={<Auth />} />
-				<Route path="/" element={<Main/>}/>		
+				<Route path="/" element={<Main/>}/>
+				<Route path="/lot/:data" element={<Lot/>}/>		
       		</Routes>
 		</main>
   </BrowserRouter>
---
+++ frontend/app/src/pages/mainPage/modules/lot.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import styles from "./lot.module.css"
+import styles1 from "../main.module.css"
+import { useNavigate } from "react-router-dom";
+import {useParams}  from "react-router-dom";
+import foto1 from "C:/Users/user/Desktop/website/website/frontend/app/src/pages/mainPage/modules/foto.jpg"
+import foto2 from "C:/Users/user/Desktop/website/website/frontend/app/src/pages/mainPage/modules/x.jpg"
+import foto3 from "C:/Users/user/Desktop/website/website/frontend/app/src/pages/mainPage/modules/y.jpg"
+
+export default function Lot(){
+    const {data} = useParams();
+    const navigate = useNavigate();
+    const [user,setUser] = useState({});
+    return(
+        <>
+       <div className={styles1.page}>
+       <header className={styles1.header}>
+        <div className={styles1.names}>
+        <div className={styles1.name}>NAME</div>
+        <div className={styles1.username}>{user ?<><button onClick={()=>{sessionStorage.removeItem("user");setUser(null)}}>Кнопка</button>{user.name}</>  : <button onClick={()=>{navigate("/auth")}}>Войти</button>}</div>
+        </div>
+        
+        </header>
+        <div className={styles1.headersize}></div>
+        <div className={styles.lot}>
+            <div className={styles.images}><img src={foto1}/></div>
+            <div className={styles.info}>
+                dfsdsds
+            </div>
+        </div>
+        <div>{data}</div>
+        <footer className={styles1.footer}>
+        </footer>
+       </div>
+        </>
+    )
+}
+
